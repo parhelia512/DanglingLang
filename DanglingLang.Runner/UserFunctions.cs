4b4e6767099182c66dcc48e0e9f6b4f17f085ac1
Alessio Parma <alessio.parma@gmail.com>
2013-06-28 15:27:56 +0200

    Broken repo, again...

--- DanglingLang.Runner/UserFunctions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DanglingLang.Runner
-{
-    static class UserFunctions
-    {
-        // It will be filled by the compiler with user defined functions,
-        // declared as static methods of this class.
-    }
-}
--- README.md
+++
@@ -1,8 +0,0 @@
-DanglingLang
-============
-
-Code is public domain.
-
-Tools used:
-* Garden Points Parser Generator: http://gppg.codeplex.com/
-* Mono.Cecil: http://www.mono-project.com/Cecil
