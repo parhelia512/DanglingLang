7f5d36a3920c7752897052a0a40bbbe61d701919
Alessio Parma <alessio.parma@gmail.com>
2013-07-05 12:03:21 +0200

    New form to quickly test code.

---
+++ DanglingLang.Gui/DanglingForm.cs
@@ -0,0 +1,18 @@
+﻿namespace DanglingLang.Gui
+{
+    using System;
+    using System.Windows.Forms;
+
+    public partial class DanglingForm : Form
+    {
+        public DanglingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DanglingForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
